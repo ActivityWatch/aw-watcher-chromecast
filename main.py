42240387ed85f02fd50bf8c622fd57c02cf824b1
Erik Bjäreholt <erik@bjareho.lt>
2017-08-02 20:32:31 +0200

    cleaned up output

--- main.py
+++ main.py
@@ -6,18 +6,49 @@ def get_devices():
 
 
 def print_cast_status(cast_status):
+    print(40 * "-")
     print("App ID: " + cast_status.app_id)
     print("Display name: " + cast_status.display_name)
     print("Status text: " + cast_status.status_text)
+    print(40 * "-")
 
 
 def print_media_status(media_status):
-    print("Title: {}".format(media_status.series_title))
-    print("Series title: {}".format(media_status.series_title))
-    print("Season: {}".format(media_status.season))
-    print("Episode: {}".format(media_status.episode))
+    print(40 * "-")
+
+    print("Title:\t\t{}".format(media_status.series_title))
+    print("Content type:\t{}".format(media_status.content_type))
+    # print("Subtitle: {}".format(media_status.subtitle))
+
+    print(40 * "-")
+
+    if media_status.media_is_generic:
+        print("Media was generic, no supported output")
+
+    if media_status.media_is_movie:
+        print("Media was movie, no supported output")
+
+    if media_status.media_is_tvshow:
+        print("Series title:\t{}".format(media_status.series_title))
+        print("Season:\t\t{}".format(media_status.season))
+        print("Episode:\t{}".format(media_status.episode))
+
+    if media_status.media_is_musictrack:
+        print("Track:\t\t{}".format(media_status.track))
+        print("Artist:\t\t{}".format(media_status.artist))
+        print("Album:\t\t{}".format(media_status.album_name))
+        print("Album artist:\t{}".format(media_status.album_artist))
+
+    if media_status.media_is_photo:
+        print("Media was photo, no supported output")
+
+    print(40 * "-")
+
     print("Player state: {}".format(media_status.player_state))
     print("Media metadata: {}".format(media_status.media_metadata))
+    print("Media custom data: {}".format(media_status.media_custom_data))
+
+    print(40 * "-")
 
 
 def main():
@@ -32,6 +63,8 @@ def main():
 
     mc = cast.media_controller
     mc.block_until_active()
+    # print(mc)
+    # print(mc.status)
     print_media_status(mc.status)
 
 
